7156ae71f3d9a29e22bf04fbfdb3d5e2f37a4e4a
Svetlana <risahawkeye@mail.ru>
2021-04-24 17:07:02 +0300

    dicstributed group search

--- SparkBayes/src/main/scala/GroupedRandomSearch.scala
+++ SparkBayes/src/main/scala/GroupedRandomSearch.scala
@@ -2,7 +2,7 @@ package sample
 
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.sql.types.{BooleanType, DataType, DoubleType, IntegerType}
+import org.apache.spark.sql.types.{BooleanType, DoubleType, IntegerType}
 import org.apache.spark.ml.odkl._
 import org.apache.spark.ml.odkl.hyperopt._
 import org.apache.spark.ml.odkl.Evaluator.TrainTestEvaluator
@@ -10,19 +10,13 @@ import org.apache.spark.ml.odkl.ModelWithSummary.Block
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.param.ParamMap
 import java.io._
 
-object BayesOpt {
-  def castAllTypedColumnsTo(df: DataFrame,
-                            columnsToBeCasted: Array[String],
-                            targetType: DataType) : DataFrame = {
-    /*
-    * Change columns datatype by name
-    */
-    columnsToBeCasted.foldLeft(df) { (foldedDf, col) =>
-      foldedDf.withColumn(col, df(col).cast(targetType))
-    }
-  }
+
+import sample.BayesOpt.castAllTypedColumnsTo
+
+object GroupedRandomSearch {
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().
@@ -30,8 +24,6 @@ object BayesOpt {
       setAppName("LearnScalaSpark")
     val sc = new SparkContext(conf)
     sc.setLogLevel("ERROR")
-
-    //println(sc.defaultParallelism)
 
     val spark = SparkSession.builder
       .master("local")
@@ -56,7 +48,6 @@ object BayesOpt {
     var preparedDF = castAllTypedColumnsTo(df, colsToInt, IntegerType)
     preparedDF = castAllTypedColumnsTo(preparedDF, colsToBool, BooleanType)
     preparedDF = castAllTypedColumnsTo(preparedDF, colsToDouble, DoubleType)
-    //preparedDF.printSchema()
 
     val featuresArray = colsNames.filter(! _.contains("label"))
     val features_assembler = new VectorAssembler()
@@ -70,71 +61,101 @@ object BayesOpt {
       .setFeatureCol("features")
       .setEta(0.1)
       .setMaxDepth(3)
-    //.setNumRounds(3)
-    //.setNumWorkers(1)
-
-    //println(model.extractParamMap())
-    //println(model.explainParams())
+      .setNumRounds(1)
+      .setNumWorkers(1)
 
     val evaluator = Evaluator.crossValidate(
       model,
       new TrainTestEvaluator(new RegressionEvaluator()),
-      //numThreads = 5,
+      numThreads = 1,
       numFolds = 3
     )
 
-
-    /*param_dist = {"max_depth": sp_randint(1, 20),
-      "min_child_weight": sp_randint(1, 20),
-      "alpha": uniform(loc=0, scale=1),
-      "lambda": uniform(loc=0, scale=1),
-      "subsample":uniform(loc=0.5, scale=0.4),
-      "colsample_bytree":uniform(loc=0.5, scale=0.4)}
-     */
-
-    val estimator = new StochasticHyperopt(evaluator)
-      .setSearchMode(BayesianParamOptimizer.GAUSSIAN_PROCESS)
+    val optimizer_first_group = new StochasticHyperopt(evaluator)
+      .setSearchMode(BayesianParamOptimizer.RANDOM)
       .setParamDomains(
         ParamDomainPair(model.maxDepth, IntRangeDomain(1, 20)),
-        ParamDomainPair(model.minChildWeight, DoubleRangeDomain(1.0, 20.0)),
+        ParamDomainPair(model.minChildWeight, DoubleRangeDomain(1.0, 20.0))
+      )
+      .setMetricsExpression("SELECT AVG(value) FROM __THIS__ WHERE metric = 'r2' AND isTest")
+      .setNumThreads(1)
+      .setMaxIter(2)//728
+      .setNanReplacement(-999)
+      .setEpsilonGreedy(0.1)
+      .setParamNames(
+        model.maxDepth -> "maxDepth",
+        model.minChildWeight -> "minChildWeight"
+      )
+
+    val optimizer_second_group = new StochasticHyperopt(evaluator)
+      .setSearchMode(BayesianParamOptimizer.RANDOM)
+      .setParamDomains(
         ParamDomainPair(model.alpha, DoubleRangeDomain(0.0, 1.0)),
-        ParamDomainPair(model.lambda, DoubleRangeDomain(0.0, 1.0)),
+        ParamDomainPair(model.lambda, DoubleRangeDomain(0.0, 1.0))
+      )
+      .setMetricsExpression("SELECT AVG(value) FROM __THIS__ WHERE metric = 'r2' AND isTest")
+      .setNumThreads(1)
+      .setMaxIter(2)//728
+      .setNanReplacement(-999)
+      .setEpsilonGreedy(0.1)
+      .setParamNames(
+        model.alpha -> "alpha",
+        model.lambda -> "lambda"
+      )
+
+    val optimizer_third_group = new StochasticHyperopt(evaluator)
+      .setSearchMode(BayesianParamOptimizer.RANDOM)
+      .setParamDomains(
         ParamDomainPair(model.subsample, DoubleRangeDomain(0.5, 0.9)),
         ParamDomainPair(model.colsampleBytree, DoubleRangeDomain(0.5, 0.9))
       )
       .setMetricsExpression("SELECT AVG(value) FROM __THIS__ WHERE metric = 'r2' AND isTest")
-      //.setNumThreads(15)
+      .setNumThreads(1)
       .setMaxIter(2)//728
       .setNanReplacement(-999)
       .setEpsilonGreedy(0.1)
+      .setParamNames(
+        model.subsample -> "subsample",
+        model.colsampleBytree -> "colsampleBytree"
+      )
+
+    val estimator = new GroupedSearch(Seq(
+      "firstGroup" -> optimizer_first_group,
+      "secondGroup" -> optimizer_second_group,
+      "thirdGroup" -> optimizer_third_group
+    ))
 
     val startTime = System.nanoTime
     val opt_result = estimator.fit(preparedDF)
     val duration = (System.nanoTime - startTime) / 1e9d
 
-    // println(opt_result.extractParamMap())
-    //println(opt_result.toString())
-
     println("configs")
     val configs = opt_result.summary(Block("configurations"))
-    configs.show(5, true)
+    configs.show(9, true)
     configs.write.format("csv")
       .option("header", "true")
-      .save("data/output/BO_configs_1.csv")
+      .save("data/output/GRS_configs_1.csv")
 
 
     println("metrics") // значения разных метрик по фолдам
     val metrics = opt_result.summary(Block("metrics"))
-    metrics.show(5, true)
+    metrics.show(9, true)
     metrics.write.format("csv")
       .option("header", "true")
-      .save("data/output/BO_metrics_1.csv")
+      .save("data/output/GRS_metrics_1.csv")
 
     println("Total time: " + duration.toString())
-    println(estimator.extractConfig(opt_result).toString())
-    val file = new File("data/output/BO_info_1.csv")
+
+//    println(optimizer_first_group.extractConfig(opt_result).toString())
+//    println(optimizer_second_group.extractConfig(opt_result).toString())
+//    println(optimizer_third_group.extractConfig(opt_result).toString())
+
+    val file = new File("data/output/GRS_info_1.csv")
     val bw = new BufferedWriter(new FileWriter(file))
-    bw.write("Total time: " + duration.toString() + "\n" + estimator.extractConfig(opt_result).toString())
+    bw.write("Total time: " + duration.toString() + "\n") // + estimator.extractConfig(opt_result).toString())
     bw.close()
   }
+
+
 }
+
--- SparkBayes/src/main/scala/RandomSearch.scala
+++ SparkBayes/src/main/scala/RandomSearch.scala
@@ -2,7 +2,7 @@ package sample
 
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.sql.types.{BooleanType, DataType, DoubleType, IntegerType}
+import org.apache.spark.sql.types.{BooleanType, DoubleType, IntegerType}
 import org.apache.spark.ml.odkl._
 import org.apache.spark.ml.odkl.hyperopt._
 import org.apache.spark.ml.odkl.Evaluator.TrainTestEvaluator
@@ -12,17 +12,9 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import java.io._
 
-object BayesOpt {
-  def castAllTypedColumnsTo(df: DataFrame,
-                            columnsToBeCasted: Array[String],
-                            targetType: DataType) : DataFrame = {
-    /*
-    * Change columns datatype by name
-    */
-    columnsToBeCasted.foldLeft(df) { (foldedDf, col) =>
-      foldedDf.withColumn(col, df(col).cast(targetType))
-    }
-  }
+import sample.BayesOpt.castAllTypedColumnsTo
+
+object MyRandomSearch {
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().
@@ -30,8 +22,6 @@ object BayesOpt {
       setAppName("LearnScalaSpark")
     val sc = new SparkContext(conf)
     sc.setLogLevel("ERROR")
-
-    //println(sc.defaultParallelism)
 
     val spark = SparkSession.builder
       .master("local")
@@ -56,7 +46,6 @@ object BayesOpt {
     var preparedDF = castAllTypedColumnsTo(df, colsToInt, IntegerType)
     preparedDF = castAllTypedColumnsTo(preparedDF, colsToBool, BooleanType)
     preparedDF = castAllTypedColumnsTo(preparedDF, colsToDouble, DoubleType)
-    //preparedDF.printSchema()
 
     val featuresArray = colsNames.filter(! _.contains("label"))
     val features_assembler = new VectorAssembler()
@@ -70,30 +59,18 @@ object BayesOpt {
       .setFeatureCol("features")
       .setEta(0.1)
       .setMaxDepth(3)
-    //.setNumRounds(3)
-    //.setNumWorkers(1)
-
-    //println(model.extractParamMap())
-    //println(model.explainParams())
+      .setNumRounds(3)
+      .setNumWorkers(1)
 
     val evaluator = Evaluator.crossValidate(
       model,
       new TrainTestEvaluator(new RegressionEvaluator()),
-      //numThreads = 5,
+      numThreads = 3,
       numFolds = 3
     )
 
-
-    /*param_dist = {"max_depth": sp_randint(1, 20),
-      "min_child_weight": sp_randint(1, 20),
-      "alpha": uniform(loc=0, scale=1),
-      "lambda": uniform(loc=0, scale=1),
-      "subsample":uniform(loc=0.5, scale=0.4),
-      "colsample_bytree":uniform(loc=0.5, scale=0.4)}
-     */
-
     val estimator = new StochasticHyperopt(evaluator)
-      .setSearchMode(BayesianParamOptimizer.GAUSSIAN_PROCESS)
+      .setSearchMode(BayesianParamOptimizer.RANDOM)
       .setParamDomains(
         ParamDomainPair(model.maxDepth, IntRangeDomain(1, 20)),
         ParamDomainPair(model.minChildWeight, DoubleRangeDomain(1.0, 20.0)),
@@ -103,7 +80,7 @@ object BayesOpt {
         ParamDomainPair(model.colsampleBytree, DoubleRangeDomain(0.5, 0.9))
       )
       .setMetricsExpression("SELECT AVG(value) FROM __THIS__ WHERE metric = 'r2' AND isTest")
-      //.setNumThreads(15)
+      .setNumThreads(3)
       .setMaxIter(2)//728
       .setNanReplacement(-999)
       .setEpsilonGreedy(0.1)
@@ -112,15 +89,12 @@ object BayesOpt {
     val opt_result = estimator.fit(preparedDF)
     val duration = (System.nanoTime - startTime) / 1e9d
 
-    // println(opt_result.extractParamMap())
-    //println(opt_result.toString())
-
     println("configs")
     val configs = opt_result.summary(Block("configurations"))
     configs.show(5, true)
     configs.write.format("csv")
       .option("header", "true")
-      .save("data/output/BO_configs_1.csv")
+      .save("data/output/RS_configs_1.csv")
 
 
     println("metrics") // значения разных метрик по фолдам
@@ -128,13 +102,15 @@ object BayesOpt {
     metrics.show(5, true)
     metrics.write.format("csv")
       .option("header", "true")
-      .save("data/output/BO_metrics_1.csv")
+      .save("data/output/RS_metrics_1.csv")
 
     println("Total time: " + duration.toString())
     println(estimator.extractConfig(opt_result).toString())
-    val file = new File("data/output/BO_info_1.csv")
+    val file = new File("data/output/RS_info_1.csv")
     val bw = new BufferedWriter(new FileWriter(file))
     bw.write("Total time: " + duration.toString() + "\n" + estimator.extractConfig(opt_result).toString())
     bw.close()
   }
+
+
 }
